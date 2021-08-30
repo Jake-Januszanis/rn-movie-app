1290aaf7ac6206bc6854fa99ed64a412613686a8
Jacob Januszanis <Jjanuszanis94@gmail.com>
2021-08-30 16:16:22 -0600

    Updated search route

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 2000;
 const axios = require('axios')
+
 require('dotenv').config();
+const PORT = process.env.PORT || 2000;
+const key = process.env.API_KEY;
 
 app.get('/', function(req,res) {
     res.send('Server Found')
 })
 
-app.get('/search', function(req,res) {
-    res.send('Searching')
+app.get('/search/:providers/:genres', function(req,res) {
+    const {genres, providers} = req.params;
+    axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&include_adult=false&with_genres=${genres},12&with_watch_providers=${providers}&sort_by=vote_average.desc&vote_count.gte=500&with_original_language=en&watch_region=US&without_genres=16, 10402`)
+        .then(function (response) {
+            res.send(response.data)
+        })
+        .catch(function (error) {
+            console.log(error)
+        })
 })
 
 
