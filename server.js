86c0524fc0727a848fae495ded15350f503f418e
Jacob Januszanis <Jjanuszanis94@gmail.com>
2021-08-31 10:59:31 -0600

    Removed axios and test route

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
-const axios = require('axios')
 const fetch = require('node-fetch')
+
+app.use(express.urlencoded({extended: true}))
+
 
 require('dotenv').config();
 const PORT = process.env.PORT || 2000;
@@ -15,18 +17,8 @@ app.get('/search/:genres/:providers', function(req,res) {
     const {genres, providers} = req.params;
     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&include_adult=false&with_genres=${genres}&with_watch_providers=${providers}&sort_by=vote_average.desc&vote_count.gte=500&with_original_language=en&watch_region=US&without_genres=16, 10402`)
     .then(res => res.json())
-    .then(data => res.send(data))   
+    .then(data => res.send(data))  
 })
-
-app.get('/test/:genres/:providers', (req, res) => {
-    const {genres, providers} = req.params;
-    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&include_adult=false&with_genres=${genres}&with_watch_providers=${providers}&sort_by=vote_average.desc&vote_count.gte=500&with_original_language=en&watch_region=US&without_genres=16, 10402`)
-    .then(res => res.json())
-    .then(data => res.send(data))   
-})
-
-
-
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
